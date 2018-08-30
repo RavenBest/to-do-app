789ae24e78b76a06bb7059429c69388d8d5bdba3
Raven Best <raven.best5@gmail.com>
2018-08-30 19:10:31 -0400

    Assignment DOM 2 completed

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 function onReady() {
   const ADD_TODO_FORM = document.getElementById('addToDoForm');
   let toDos = [];
+  let id = 0;
 
   function renderTheUI(){
     const TODO_LIST = document.getElementById('toDoList');
     TODO_LIST.textContent = '';
-    
+
       toDos.forEach(function(toDo){
         const NEW_LI = document.createElement('li');
         const CHECKBOX = document.createElement('input');
         CHECKBOX.type = "checkbox";
 
+        const DELETE_BTN = document.createElement('button');
+        DELETE_BTN.textContent = "Delete!";
+
+        DELETE_BTN.addEventListener('click', event => {
+          toDos = toDos.filter(function(item){
+            return item.id !== toDo.id;
+          });
+
+          renderTheUI();
+        });
+
         NEW_LI.textContent = toDo.title;
+
         NEW_LI.appendChild(CHECKBOX);
         TODO_LIST.appendChild(NEW_LI);
+        NEW_LI.appendChild(DELETE_BTN);
     });
   }
 
@@ -24,6 +38,8 @@ function onReady() {
       title: NEW_TODO_TEXT.value,
       complete: false
     });
+
+    id++;
 
     NEW_TODO_TEXT.value = '';
     renderTheUI();
